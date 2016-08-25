6d6259ad1655c0485ddc263174efd85142283da3
Silas Reinagel <silas.reinagel@gmail.com>
2016-08-25 02:25:36 -0700

    Removed a 'new' that was not in a secondary constructor

--- src/oo/pure/core/iteration/IntRange.java
+++ src/oo/pure/core/iteration/IntRange.java
@@ -4,22 +4,31 @@ import oo.pure.core.number.Int;
 import oo.pure.core.number.IntValue;
 
 import java.util.Iterator;
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public final class IntRange implements Iterable<IntValue>
 {
     private final IntValue _firstValue;
     private final IntValue _lastValue;
+    private final Function<Integer, IntValue> _toIntValue;
 
-    public IntRange(final IntValue firstValue, IntValue lastValue)
+    public IntRange(final IntValue firstValue, final IntValue lastValue)
+    {
+        this(firstValue, lastValue, x -> new Int(x));
+    }
+
+    public IntRange(final IntValue firstValue, final IntValue lastValue, final Function<Integer, IntValue> toIntValue)
     {
         _firstValue = firstValue;
         _lastValue = lastValue;
+        _toIntValue = toIntValue;
     }
 
     @Override
     public Iterator<IntValue> iterator()
     {
-        return IntStream.rangeClosed(_firstValue.intValue(), _lastValue.intValue()).mapToObj(x -> (IntValue)new Int(x)).iterator();
+        return IntStream.rangeClosed(_firstValue.intValue(), _lastValue.intValue())
+                .mapToObj(x -> _toIntValue.apply(x)).iterator();
     }
 }
